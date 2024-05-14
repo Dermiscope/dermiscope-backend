ea6215519f40f6dc1f41603b15cb5387a4d3eb90
aristocaesar <hi@aristoc.space>
2024-05-14 15:11:57 +0700

    [REFACTORY] - inti sequelize orm, refactory structure folder

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { port } = require('./core/config');
+const server = require('./core/server');
+const { logger } = require('./core/logger');
+
+// App server listen
+const app = server.listen(port, (err) => {
+  if (err) {
+    process.exit(1);
+  } else {
+    logger.info(`Server Running On Port ${port}`);
+  }
+});
+
+// Handle Uncaught Exception Error
+process.on('uncaughtException', (err) => {
+  logger.error(err, 'Uncaught exception.');
+  app.close(() => process.exit(1));
+
+  setTimeout(() => process.abort(), 1000).unref();
+  process.exit(1);
+});
